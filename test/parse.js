f0f21df3441c71d4fac0cfc37167c1e71ad32040
Matt Carter <m@ttcarter.com>
2024-06-29 15:58:04 +1000

    refactor: Use proper exec matching + stacking

--- test/parse.js
+++ test/parse.js
@@ -3,25 +3,49 @@ import parse from '#lib/parse';
 
 describe('cacx.parse()', ()=> {
 
-	it('simple inputs #1 ', ()=>
-		expect(parse('<a>Value A</a>')).to.deep.equal([
-			{tag: 'a', text: 'Value A'},
-		])
+	it('simple inputs - single tag', ()=>
+		expect(parse('<a>Value A</a>')).to.deep.equal({
+			tag: 'a',
+			text: 'Value A'
+		})
 	);
 
-	it('simple inputs #2 ', ()=>
-		expect(parse('<a><b>Value B</b></a>')).to.deep.equal([
-			{tag: 'a'},
-			{tag: 'b', text: 'Value B'},
-		])
+	it('simple inputs - two level nested', ()=>
+		expect(parse('<a><b>Value B</b></a>')).to.deep.equal({
+			tag: 'a',
+			children: [{
+				tag: 'b',
+				text: 'Value B',
+			}],
+		})
 	);
 
-	it('simple inputs #3 ', ()=>
-		expect(parse('<a><b><c>Value C</c></b></a>')).to.deep.equal([
-			{tag: 'a'},
-			{tag: 'b'},
-			{tag: 'c', text: 'Value C'},
-		])
+	it('simple inputs - three level nesting', ()=>
+		expect(parse('<a><b><c>Value C</c></b></a>')).to.deep.equal({
+			tag: 'a',
+			children: [{
+				tag: 'b',
+				children: [{
+					tag: 'c',
+					text: 'Value C',
+				}],
+			}],
+		})
 	);
+
+	it('simple inputs - nesting with prefix / suffix + value', ()=>
+		expect(parse('<a>AP:<b>BP:<c>VC</c>BS</b>AS</a>')).to.deep.equal({
+			tag: 'a',
+			text: 'AP:AS',
+			children: [{
+				tag: 'b',
+				text: 'BP:BS',
+				children: [{
+					tag: 'c',
+					text: 'VC',
+				}],
+			}],
+		})
+	)
 
 });
