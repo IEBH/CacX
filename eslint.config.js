80a897b77681adf6df066a8288a6b2e101afa9cf
Matt Carter <m@ttcarter.com>
2025-02-26 13:34:20 +1000

    chore: Added Eslint to project + various misc code cleanups

--- eslint.config.js
+++ eslint.config.js
@@ -2,11 +2,9 @@ import RulesMFDC from '@momsfriendlydevco/eslint-config';
 
 export default [
 	{
-		// Global ignore rules - Do not add any other keys to this object or eslint doesn't treat this as global
+		// Global ignore rules - Do not add any other keys to this object
 		ignores: [
 			'.*',
-			'docs/',
-			'dist/',
 			'node_modules/',
 		],
 	},
